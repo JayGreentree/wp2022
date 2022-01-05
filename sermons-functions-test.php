fd6fa13708c25d5e69265b212a747d7ddc3c1027
Jay Greentree <JayGreentree@users.noreply.github.com>
2022-01-04 23:01:02 -0500

    Update sermons-functions-test.php

--- sermons-functions-test.php
+++ sermons-functions-test.php
@@ -1,4 +1,5 @@
-			if( function_exists('acf_add_local_field_group') ):
+<?php
+if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
 	'key' => 'group_61d4aea5c270e',
