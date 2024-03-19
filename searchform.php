005a56969bab39910964941a83a606609bfdb094
Jay Greentree <JayGreentree@users.noreply.github.com>
2024-03-18 22:07:16 -0400

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -6,7 +6,13 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div class="form-group">
 		<label for="s" class="assistive-text"><?php _e( 'Search', 'MVBlue' ); ?></label>
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'MVBlue' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'MVBlue' ); ?>" />
+	</div>
+	<div class="form-group">
+		<input type="text" class="form-control" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'MVBlue' ); ?>">
+	</div>
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'MVBlue' ); ?>">
+	</div>
 	</form>
